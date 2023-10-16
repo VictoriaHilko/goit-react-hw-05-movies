509b6fb816c8882654414757f78b931bcfc5bd55
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-10-16 16:28:16 +0300

    add Code Splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { Home } from "pages/Home/Home";
-import { Movies } from "pages/Movies/Movies";
+// import { Movies } from "pages/Movies/Movies";
 import { NotFound } from "pages/NotFound/NotFound";
 import { MovieDetails } from "pages/MovieDetails/MovieDetails";
-import { Cast } from "./Cast/Cast";
-import { Reviews } from "./Reviews/Reviews";
+import { lazy } from "react";
+// import { Cast } from "./Cast/Cast";
+// import { Reviews } from "./Reviews/Reviews";
+
+
+const Movies = lazy(() =>
+  import('../pages/Movies/Movies').then(module => ({
+    ...module,
+    default: module.Movies,
+  }))
+);
+
+const Cast = lazy(() =>
+  import('./Cast/Cast').then(module => ({
+    ...module,
+    default: module.Cast,
+  }))
+);
+
+const Reviews = lazy(() =>
+  import('./Reviews/Reviews').then(module => ({
+    ...module,
+    default: module.Reviews,
+  }))
+);
 
 
 export const App = () => {
